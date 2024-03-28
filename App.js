5ac7112905551d2ed2079b21ff31b3474036313c
ShurikFW <aleksandrmastakov2001@gmail.com>
2024-03-28 15:52:24 +0200

    Fix import

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import { ImageViewer } from './components/ImageViewer';
+import  ImageViewer  from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
