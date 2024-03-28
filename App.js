c70c58d0f5e406f599c05d8f8cdd8b44fc0e7cda
Vladimir Drozdik <vladimir@codeborne.com>
2024-03-28 15:45:03 +0200

    extract image into separate component

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+import { ImageViewer } from './components/ImageViewer';
+
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={PlaceholderImage} style={styles.Image} />
+      <View style={styles.imageContainer}>
+      <ImageViewer placeholderImageSource={PlaceholderImage}/> 
       <Text style={{ color: '#fff'}}>
         Open up VovaN to start working on your app!</Text>
       <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
