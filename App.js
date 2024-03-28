07fb35c900730bb11f29b46bb3e96d075edaf015
ShurikFW <aleksandrmastakov2001@gmail.com>
2024-03-28 14:46:20 +0200

    second commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={PlaceholderImage} style={styles.Image} />
+      <Text style={{ color: '#fff'}}>
+        Open up Sanja to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
